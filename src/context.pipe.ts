ab076f8ad2de3a2a45ec7729687aa9e1402f6bde
zinzinday <zinzinday@gmail.com>
2019-05-23 20:14:52 +0700

    Fix

--- src/context.pipe.ts
+++ src/context.pipe.ts
@@ -6,14 +6,13 @@ import {TransformOptions} from "class-transformer";
 @Injectable()
 export class ContextPipe implements PipeTransform {
     options: TransformOptions;
-
     constructor(public context?: ContextService) {
-        if (context && context.req && context.req.user) {
-            this.options = {groups: [context.req.user.role]};
-        }
     }
 
     transform(value, metadata: ArgumentMetadata) {
+        if (this.context && this.context.req && this.context.req.user) {
+            this.options = {groups: [this.context.req.user.role]};
+        }
         const {metatype, type} = metadata;
         if (['body', 'query'].indexOf(type) !== -1) {
             return transform(value, metatype, this.options);
